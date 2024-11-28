b85cd78a4a37e973b0460b7bf8d594ea7e21d27a
AdrianeDriane <dilaoaparre@gmail.com>
2024-11-28 16:50:49 +0800

    First Commit

---
+++ src/main/java/com/g4bsit2a/boardingpal/BoardingPal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.g4bsit2a.boardingpal;
+
+/**
+ *
+ * @author Adriane Dilao
+ */
+public class BoardingPal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
